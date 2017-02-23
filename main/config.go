adfbab0b8ef7f368f38a7e7d2694f46aa8cba29a
Hans Jorgen Nygaardshaug <hans.nyg@gmail.com>
2017-02-23 09:46:36 +0100

    Config file

---
+++ main/config.go
@@ -0,0 +1,11 @@
+package main
+
+const (
+	HOST_IP    = ""          //Interface to listen to. Leave blank for all.
+	HOST_PORT  = "7171"      //Port to liten on
+	MYSQL_USER = ""          //Username to mysql login
+	MYSQL_PW   = ""          //Password to mysql login
+	MYSQL_IP   = "127.0.0.1" //Loopback default
+	MYSQL_PORT = "3306"      //MySql default
+	MYSQL_DB   = "gotibia"   //MySql database name
+)
